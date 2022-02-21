d7dd7e1707e8854d7bf61eee943805126f75684e
ifeoluwa.afolabi <ifeoluwaflb@gmail.com>
2022-02-21 20:53:45 +0100

    2nd Commit

--- src/student-registration/users/users.module.ts
+++ src/student-registration/users/users.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Module({
-  controllers: [UsersController],
-  providers: [UsersService]
+imports: [TypeOrmModule.forFeature([User])],
+controllers: [UsersController],
+providers: [UsersService]
 })
 export class UsersModule {}
+
+
