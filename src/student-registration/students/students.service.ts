33fcc352b5abb8eccc525cf5eb5dc35f60e4eb48
ifeoluwa.afolabi <ifeoluwaflb@gmail.com>
2022-02-23 16:10:58 +0100

    Final Commit

--- src/student-registration/students/students.service.ts
+++ src/student-registration/students/students.service.ts
@@ -1,11 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../users/entities/user.entity';
+import { Student } from './entities/student.entity';
+import { Repository } from 'typeorm';
 import { CreateStudentDto } from './dto/create-student.dto';
 import { UpdateStudentDto } from './dto/update-student.dto';
 
 @Injectable()
 export class StudentsService {
-  create(createStudentDto: CreateStudentDto) {
-    return 'This action adds a new student';
+
+  constructor(
+    @InjectRepository(Student)
+    private studentRepository: Repository<Student>,
+
+    @InjectRepository(User)
+    private userRepository: Repository<User>
+  ) { }
+
+  async create(createStudentDto: CreateStudentDto) {
+    //return 'This action adds a new student';
+    const newStudent = this.studentRepository.create(createStudentDto);
+    //ideally, below should be wrapped in a transaction so that it can roll back if there is error in any of the stages.
+    if (createStudentDto.user) {
+      const newUser = this.userRepository.create(createStudentDto.user);
+      const user: User = await this.userRepository.save(newUser);
+      newStudent.user = user;
+    }
+    return this.studentRepository.save(newStudent)
   }
 
   findAll() {
